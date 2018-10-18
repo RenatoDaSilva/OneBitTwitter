077dbcff74b351f81c1e0979b3fd54d852c7be7f
RenatoDaSilva <renato@asseinfo.com.br>
2018-10-18 18:37:09 -0300

    18-10-2018

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,8 +1,8 @@
 module Api
-   module V1
-      class ApiController < ApplicationController
-         #> Métodos globais
-      end
-   end
+  module V1
+    class ApiController < ApplicationController
+      include Knock::Authenticable
+      # > Métodos globais
+    end
+  end
 end
-
